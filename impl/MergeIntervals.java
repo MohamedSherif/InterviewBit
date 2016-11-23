aa1447b62313e5b9b83968567e3746d0f478adb4
Mohamed Sherif <mohamed.sherif.2012@gmail.com>
2016-11-23 11:29:22 +0200

    Merge Overlapping Interval Problem and Merge non-overlapping Intervals Problem

---
+++ impl/MergeIntervals.java
@@ -0,0 +1,66 @@
+package impl;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class MergeIntervals {
+
+	
+	public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+
+        ArrayList<Interval> result = new ArrayList<Interval>();
+ 
+        if(intervals==null||intervals.size()==0)
+            return result;
+     
+        Collections.sort(intervals, new Comparator<Interval>(){
+            public int compare(Interval i1, Interval i2){
+                if(i1.start!=i2.start)
+                    return i1.start-i2.start;
+                else
+                    return i1.end-i2.end;
+            }
+        });
+     
+        
+        Interval pre = intervals.get(0);
+        
+        for(int i=0; i<intervals.size(); i++){
+            Interval curr = intervals.get(i);
+            if(curr.start>pre.end){
+                result.add(pre);
+                pre = curr;
+            }else{
+                Interval merged = new Interval(pre.start, Math.max(pre.end, curr.end));
+                pre = merged;
+            }
+        }
+        result.add(pre);
+
+        return result;
+    }
+	
+	
+	public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
+		 
+        ArrayList<Interval> result = new ArrayList<Interval>();
+ 
+        for(Interval interval: intervals){
+            if(interval.end < newInterval.start){
+                result.add(interval);
+            }else if(interval.start > newInterval.end){
+                result.add(newInterval);
+                newInterval = interval;        
+            }else if(interval.end >= newInterval.start || interval.start <= newInterval.end){
+                newInterval = new Interval(Math.min(interval.start, newInterval.start), Math.max(newInterval.end, interval.end));
+            }
+        }
+ 
+        result.add(newInterval); 
+ 
+        return result;
+    }
+
+	
+}
