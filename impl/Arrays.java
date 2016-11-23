c4d5a7effc815ee9d4ebf5d53cc031b6aba926d8
Mohamed Sherif <mohamed.sherif.2012@gmail.com>
2016-11-23 16:11:33 +0200

    Set Matrix Zeroes Problem

--- impl/Arrays.java
+++ impl/Arrays.java
@@ -54,31 +54,6 @@ public class Arrays {
 			}
 		}
 		
-		
-		/*
-		 * for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[0][j] = 0;
-                    matrix[i][0] = 0;
-                }
-            }
-        }
-        for (int i = 1; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                for (int j = 1; j < n; j++)
-                    matrix[i][j] = 0;
-            }
-        }
-        for (int i = 1; i < n; i++) {
-            if (matrix[0][i] == 0) {
-                for (int j = 1; j < m; j++)
-                    matrix[j][i] = 0;
-            }
-        }
-        if (firstRowZero) 
-		 */
-		
 		for (int i = 1; i < m; i++) {
 			for (int j = 1; j < n; j++) {
 				if(matrix[i][j] == 0){
