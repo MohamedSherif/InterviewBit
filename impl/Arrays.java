6e8338d9fa29f27d61ef9478113aeb3d69635235
Mohamed Sherif <mohamed.sherif.2012@gmail.com>
2016-11-23 16:05:28 +0200

    Set Matrix Zeroes Problem

--- impl/Arrays.java
+++ impl/Arrays.java
@@ -21,10 +21,112 @@ public class Arrays {
 		}
 		
 		for (int i = 0; i < n; i++) {
-			
+			if(A[i] != i){
+				return i;
+			}
 		}
 		
 		return n;
 	}
 	
+	
+	public static int[][] setMatrixZeroes(int[][] matrix){
+		
+		boolean zeroesInFirstRow = false;
+		boolean zeroesInFirstCol = false; 
+		
+		int m = matrix.length;
+		int n = matrix[0].length;
+		
+		//Loop on the first Column 
+		for (int i = 0; i < m; i++) {
+			if(matrix[i][0] == 0){
+				zeroesInFirstCol = true;
+				break;
+			}
+		}
+		
+		//Loop over the first Row
+		for (int i = 0; i < n; i++) {
+			if (matrix[0][i] == 0) {
+				zeroesInFirstRow = true;
+				break;
+			}
+		}
+		
+		
+		/*
+		 * for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; j++)
+                    matrix[i][j] = 0;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                for (int j = 1; j < m; j++)
+                    matrix[j][i] = 0;
+            }
+        }
+        if (firstRowZero) 
+		 */
+		
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				if(matrix[i][j] == 0){
+					matrix[i][0] = 0;
+					matrix[0][j] = 0;
+				}
+			}
+		}
+		
+		for (int i = 1; i < m; i++) {
+			if(matrix[i][0] == 0){
+				for (int j = 1; j < n; j++) {
+					matrix[i][j] = 0;
+				}
+			}
+		}
+		
+		for (int i = 1; i < n; i++) {
+			if(matrix[0][i] == 0){
+				for (int j = 1; j < m; j++) {
+					matrix[j][i] = 0;
+				}
+			}
+		}
+		
+		if(zeroesInFirstCol){
+			for (int i = 0; i < m; i++) {
+				matrix[i][0] = 0;
+			}
+		}
+		
+		if(zeroesInFirstRow){
+			for (int i = 0; i < n; i++) {
+				matrix[0][i] = 0;
+			}
+		}
+		
+		return matrix;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		int[][] a = {{0, 1},
+				  {1, 1}};
+		
+		
+		setMatrixZeroes(a);
+		
+	}
 }
