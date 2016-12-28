87d0872ac699b3ba8bb4f09dbc221916d94fb897
Mohamed Sherif <mohamed.sherif.2012@gmail.com>
2016-12-28 22:48:19 +0200

    Excel Column Number Problem

--- impl/MathsSolver.java
+++ impl/MathsSolver.java
@@ -231,11 +231,9 @@ public class MathsSolver {
 	 * 
 	 * Example :
 	 * 
-	 * 	Input : 12121 
-	 * Output : True
+	 * Input : 12121 Output : True
 	 * 
-	 * 	Input : 123 
-	 * Output : False See Expected Output
+	 * Input : 123 Output : False See Expected Output
 	 * 
 	 * @param a
 	 * @return
@@ -286,6 +284,58 @@ public class MathsSolver {
 	}
 	// ******************************************************************
 
+	/**
+	 * Excel Column Number:
+	 * 	Given a column title as appears in an Excel sheet, return its
+	 * 	corresponding column number.
+	 * 
+	 * Example:
+	 * 		A -> 1
+	 * 		B -> 2
+	 * 		C -> 3
+	 * 		...
+	 * 		Z -> 26
+	 * 		AA -> 27
+	 * 		AB -> 28
+	 * 
+	 * @param A
+	 * @return
+	 */
+	public static int excelColumnNumber(String A) {
+		int n = A.length();
+
+		int value = 0;
+		for (int i = 0; i < n; i++) {
+			value += Math.pow(26, i) * (A.charAt(n - (i + 1)) - 'A' + 1);
+		}
+		return value;
+	}
+
+	public int titleToNumber(String A) {
+		int[] val;
+
+		val = new int[26];
+
+		for (int i = 0; i < 26; i++) {
+			val[i] = i + 1;
+		}
+
+		int ten = 1;
+		int value = 0;
+
+		for (int i = A.length() - 1; i >= 0; i--) {
+			char c = A.charAt(i);
+			value += (ten * val[c - 'A']);
+			ten *= 26;
+		}
+
+		return value;
+	}
+//**************************************************************
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// fillPrimes(25);
 		// for (int i = 0; i < isPrime.length; i++) {
@@ -297,7 +347,10 @@ public class MathsSolver {
 		// System.out.println(getGcd(54, 24));
 		// System.out.println(System.currentTimeMillis());
 
-		System.out.println(isPalindromeNum(2147447412));
+		// System.out.println(isPalindromeNum(2147447412));
+
+		System.out.println(excelColumnNumber("AB"));
+
 	}
 
 }
