fc8b0624d975f8b7c18954ed5f9baad83f01b887
Mohamed Sherif <mohamed.sherif.2012@gmail.com>
2017-01-20 01:04:18 +0200

    Integer to Roman - InterViewBit

--- impl/StringsSolver.java
+++ impl/StringsSolver.java
@@ -83,10 +83,44 @@ public class StringsSolver {
 
 //	============================================================
 	
+	/**
+	 * Given an integer, convert it to a roman numeral, and return a string
+	 * corresponding to its roman numeral version
+	 * 
+	 * Input is guaranteed to be within the range from 1 to 3999.
+	 * 
+	 * Example :
+	 * 	- Input : 5 
+	 * 	- Return : "V"
+	 * 
+	 * 	- Input : 14 
+	 * 	- Return : "XIV"
+	 * 
+	 * @param num
+	 * @return
+	 */
+	public static String intToRoman(int num) {
+		StringBuilder sb = new StringBuilder();
+		
+		int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+	    String[] strs = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+	    
+	    for(int i=0;i<values.length;i++) {
+	        while(num >= values[i]) {
+	            num -= values[i];
+	            sb.append(strs[i]);
+	        }
+	    }
+	    
+		return sb.toString();
+	}
+	
+//	===============================================
+	
 	
 	
 	
 	public static void main(String[] args) {
-
+		System.out.println(intToRoman(14));
 	}
 }
