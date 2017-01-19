7ecf26ebd9f33968eb9a4a26a9ae44b1e6d0f607
Mohamed Sherif <mohamed.sherif.2012@gmail.com>
2017-01-20 00:23:49 +0200

    Roman to Integer Problem - InterviewBit

--- impl/StringsSolver.java
+++ impl/StringsSolver.java
@@ -8,7 +8,78 @@ package impl;
  */
 public class StringsSolver {
 
+	/**
+	 * Given a roman numeral, convert it to an integer.
+	 * 
+	 * Input is guaranteed to be within the range from 1 to 3999.
+	 * 
+	 * Read more details about roman numerals at Roman Numeric System
+	 * 
+	 * Example :
+	 * 
+	 * Input : "XIV" Return : 14 Input : "XX" Output : 20
+	 * 
+	 * @param a
+	 * @return
+	 */
+	public static int romanToInt(String a) {
+		int decimal = 0;
+		int lastNumber = 0;
+		String romanNumeral = a.toUpperCase();
+		/*
+		 * operation to be performed on upper cases even if user enters roman
+		 * values in lower case chars
+		 */
+		for (int x = romanNumeral.length() - 1; x >= 0; x--) {
+			char convertToDecimal = romanNumeral.charAt(x);
 
+			switch (convertToDecimal) {
+			case 'M':
+				decimal = processDecimal(1000, lastNumber, decimal);
+				lastNumber = 1000;
+				break;
+
+			case 'D':
+				decimal = processDecimal(500, lastNumber, decimal);
+				lastNumber = 500;
+				break;
+
+			case 'C':
+				decimal = processDecimal(100, lastNumber, decimal);
+				lastNumber = 100;
+				break;
+
+			case 'L':
+				decimal = processDecimal(50, lastNumber, decimal);
+				lastNumber = 50;
+				break;
+
+			case 'X':
+				decimal = processDecimal(10, lastNumber, decimal);
+				lastNumber = 10;
+				break;
+
+			case 'V':
+				decimal = processDecimal(5, lastNumber, decimal);
+				lastNumber = 5;
+				break;
+
+			case 'I':
+				decimal = processDecimal(1, lastNumber, decimal);
+				lastNumber = 1;
+				break;
+			}
+		}
+		return decimal;
+	}
+
+	public static int processDecimal(int decimal, int lastNumber, int lastDecimal) {
+		if (lastNumber > decimal) {
+			return lastDecimal - decimal;
+		} else {
+			return lastDecimal + decimal;
+		}
+	}
 
 //	============================================================
 	
