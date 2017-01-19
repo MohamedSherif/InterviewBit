1d7a639910807f9ede09fc04a44b99aae093dbb8
Mohamed Sherif <mohamed.sherif.2012@gmail.com>
2017-01-20 00:17:48 +0200

    Adding Class for the Srings Solutions

---
+++ impl/StringsSolver.java
@@ -0,0 +1,21 @@
+package impl;
+
+/**
+ * A class that contains solutions for all problem of the Strings part of level 3.
+ * 
+ * @author Mohamed Sherif
+ *
+ */
+public class StringsSolver {
+
+
+
+//	============================================================
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+	}
+}
