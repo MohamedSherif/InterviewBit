9971b2af125d59751970f47451e0222967a31b59
Mohamed Sherif <mohamed.sherif.2012@gmail.com>
2017-01-20 02:32:34 +0200

    Palindrome Problem - InterviewBit

--- impl/StringsSolver.java
+++ impl/StringsSolver.java
@@ -118,9 +118,40 @@ public class StringsSolver {
 //	===============================================
 	
 	
+	/**
+	 * Given a string, determine if it is a palindrome, considering only
+	 * alphanumeric characters and ignoring cases.
+	 * 
+	 * Example:
+	 * 
+	 * "A man, a plan, a canal: Panama" is a palindrome.
+	 * 
+	 * "race a car" is not a palindrome.
+	 * 
+	 * Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public static int isPalindrome(String s) {
+	    int isPalindrome = 1;
+		
+		String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+			
+		if (!actual.equalsIgnoreCase("")) {	
+			for (int i = 0; i <= actual.length()/2; i++) {
+				if(actual.charAt(i) != actual.charAt(actual.length()-1-i))
+					return 0;
+			}
+		}
+		
+		return isPalindrome;
+	}
 	
 	
 	public static void main(String[] args) {
 		System.out.println(intToRoman(14));
+		
+		System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
 	}
 }
