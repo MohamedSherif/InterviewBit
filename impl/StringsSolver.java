b95ae2ad4cb1df820fddaee1f34da0b09f218470
Mohamed Sherif <mohamed.sherif.2012@gmail.com>
2017-01-20 04:22:06 +0200

    Reverse String Problem - InterviewBit

--- impl/StringsSolver.java
+++ impl/StringsSolver.java
@@ -184,9 +184,49 @@ public class StringsSolver {
 		return stArr[stArr.length - 1].length();
 	}
 	
+	
+	
+//	==========================
+	
+	/**
+	 * Given an input string, reverse the string word by word.
+	 * 
+	 * Example:
+	 * 
+	 * Given s = "the sky is blue",
+	 * 
+	 * return "blue is sky the".
+	 * 
+	 * A sequence of non-space characters constitutes a word. Your reversed
+	 * string should not contain leading or trailing spaces, even if it is
+	 * present in the input string. If there are multiple spaces between words,
+	 * reduce them to a single space in the reversed string
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public static String reverseWords(String s) {
+		StringBuilder sb = new StringBuilder();
+	    s = s.replaceAll("\\s+", " ");
+	    String[] strArr = s.split(" ");
+	    
+	    for (int i = strArr.length-1; i >= 0; i--) {
+	    	if(!strArr[i].equalsIgnoreCase(" ") && !strArr[i].equalsIgnoreCase("")){
+	    		sb.append(strArr[i]);
+		    	if (i != 0) {
+		    		sb.append(" ");
+				}
+	    	}
+    	}
+		return sb.toString().trim();
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println(intToRoman(14));
 		
 		System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+		
+		System.out.println(reverseWords(" ankrqzzcel  dyaiug y rkicv t"));
 	}
 }
